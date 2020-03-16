4d47f963efd4ab8d9b9f6ea2eb8bdda9df311cbd
ABHIJITH SURENDRAN <41501829+ABHJITH@users.noreply.github.com>
2020-03-16 11:08:15 +0530

    Add files via upload

---
+++ src/app/apod-response.ts
@@ -0,0 +1,21 @@
+import { Url } from 'url';
+
+
+
+export interface ApodResponse {
+
+
+    
+    Search: JSON,
+    totalResults:string,
+    Response: string,
+    Title:string,
+    Year:string,
+    Type: string,
+    imdbID: string,
+    Poster:Url
+
+    
+   
+
+}
